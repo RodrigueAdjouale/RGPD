96edec1a5d4ed82ec996dabbec1cc5e8c8771917
R31 <rodrigue@adjouale.com>
2018-11-25 09:26:32 +0100

    création composant Entete

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text} from 'react-native';
+import Entete from './components/entete';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Text><Entete /></Text>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/entete/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Entete = ()=> (
+  <Text> Mes Contacts</Text>;
+)
+
+  export default Entete;
